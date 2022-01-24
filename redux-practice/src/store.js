dac2d7e448b028fdaf0d0582e0845b0912fd1778
apdltpdl22 <apdltpdl22@naver.com>
2022-01-25 01:12:01 +0900

    4-5 slice

--- redux-practice/src/store.js
+++ redux-practice/src/store.js
@@ -1,29 +1,5 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// const addToDo = createAction("ADD")
-// const deleteToDo = createAction("DELETE")
-
-
-// const reducer = (state = [], action) => {
-//   switch(action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case deleteToDo.type:
-//       return state.filter(toDo => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
-
-// const reducer = createReducer([], {
-//   // return하지 않음. return은 무조건 새로운 state로만
-//   [addToDo] : (state, action) => {
-//     state.push({ text: action.payload, id: Date.now() })
-//   },
-//   // return 하고 있음.
-//   [deleteToDo] : (state, action) => state.filter(toDo => toDo.id !== action.payload)
-// });
-
 const toDos = createSlice({
   name:'toDosReducer',
   initialState: [],
@@ -36,8 +12,5 @@ const toDos = createSlice({
 });
 
 
-const store = configureStore({reducer:toDos.reducer})
 export const { add, remove } = toDos.actions
-
-
-export default store;
+export default configureStore({reducer:toDos.reducer});
