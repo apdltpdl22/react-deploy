6b53ef44ba9ec40271dd11ef1646fc30faebdfcb
apdltpdl22 <apdltpdl22@naver.com>
2022-01-25 01:05:47 +0900

    4-1

--- redux-practice/src/store.js
+++ redux-practice/src/store.js
@@ -1,39 +1,43 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// const addToDo = createAction("ADD")
+// const deleteToDo = createAction("DELETE")
 
-const addToDo = text => {
-  return {
-    type: ADD,
-    text
+
+// const reducer = (state = [], action) => {
+//   switch(action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter(toDo => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+// const reducer = createReducer([], {
+//   // return하지 않음. return은 무조건 새로운 state로만
+//   [addToDo] : (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() })
+//   },
+//   // return 하고 있음.
+//   [deleteToDo] : (state, action) => state.filter(toDo => toDo.id !== action.payload)
+// });
+
+const toDos = createSlice({
+  name:'toDosReducer',
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() })
+    },
+    remove: (state, action) => state.filter(toDo => toDo.id !== action.payload)
   }
-}
+});
 
-const deleteToDo = id => {
-  return {
-    type: DELETE,
-    id: parseInt(id)
-  }
-}
 
-const reducer = (state = [], action) => {
-  switch(action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter(toDo => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer)
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo
-}
+const store = configureStore({reducer:toDos.reducer})
+export const { add, remove } = toDos.actions
 
 
 export default store;
