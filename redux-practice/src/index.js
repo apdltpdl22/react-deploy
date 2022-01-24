38dcf8794d36ea74dae939218c6fc6e2da7e2127
apdltpdl22 <apdltpdl22@naver.com>
2022-01-24 21:00:34 +0900

    1-3

--- redux-practice/src/index.js
+++ redux-practice/src/index.js
@@ -6,6 +6,7 @@ const minus = document.getElementById("minus")
 
 number.innerText = 0;
 
+//reducer
 const countModifier = (count = 0, action) => {
   if (action.type === "ADD") {
     return count + 1;
@@ -22,6 +23,7 @@ const onChange = () => {
 }
 countStore.subscribe(onChange);
 
+// Action은 반드시 object여야 하고 'type:'으로 시작
 const handleAdd = () => {
   countStore.dispatch({ type: "ADD" })
 }
