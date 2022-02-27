e4dfb940ac5c4a5dba4826dc167fd029cca2010c
apdltpdl22 <apdltpdl22@naver.com>
2022-02-27 12:18:41 +0900

    css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,24 @@
 import propTypes from 'prop-types';
 import PopTypes from 'prop-types';
 import { Link } from 'react-router-dom'
+import styles from "./Movie.module.css";
 
-function Movie({coverImg, title, summary, genres, id}) {
+function Movie({coverImg, title, summary, genres, id, year}) {
   return (
-    <div>
-      <img src={coverImg} alt={title}/>
-      <h2>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img}/>
+      <h2 className={styles.movie__title}>
         <Link to ={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-        <li key={g}>{g}</li>
-        ))}
+      <h3 className={styles.movie__year}>{year}</h3>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...`: summary}</p>
+      <ul className={styles.movie__genres}>
+        {genres.length > 1 ?
+        (genres.map((g) => (
+          <li key={g}>{g}</li>
+          ))) :
+        <li>{genres}</li>
+        }        
       </ul>
       </div>
     )
@@ -24,6 +29,6 @@ function Movie({coverImg, title, summary, genres, id}) {
     coverImg : propTypes.string.isRequired,
     title : propTypes.string.isRequired,
     summary : propTypes.string.isRequired,
-    genres: propTypes.arrayOf(PopTypes.string).isRequired
+    // genres: propTypes.arrayOf(PopTypes.string).isRequired
   };
 export default Movie;
